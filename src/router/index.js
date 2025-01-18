a91290e7d5ea438b68c7a35d14dfbcea44a129b7
neve99 <165163178+neve99@users.noreply.github.com>
2025-01-18 17:14:54 +0100

    set up scaffolded codebase and vue router

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+  { path: '/', component: () => import('../views/Home.vue') },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) return savedPosition
+    // return { top: 0 }
+    // time out to wait for the transition
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve({ top: 0, behavior: 'smooth' })
+      }, 300)
+    })
+  }
+})
+
+export default router
