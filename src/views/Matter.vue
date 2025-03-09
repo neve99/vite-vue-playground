e1db2eb26b496d51eeee7bc763300da9512f25db
neve99 <165163178+neve99@users.noreply.github.com>
2025-03-09 14:44:01 +0100

    get rid of debug div

--- src/views/Matter.vue
+++ src/views/Matter.vue
@@ -342,7 +342,7 @@ onMounted(() => {
             console.log('Permission granted!');
             
             // Add a debug display
-            const debugDiv = document.createElement('div');
+            /* const debugDiv = document.createElement('div');
             debugDiv.style.position = 'fixed';
             debugDiv.style.top = '10px';
             debugDiv.style.left = '10px';
@@ -351,12 +351,12 @@ onMounted(() => {
             debugDiv.style.padding = '5px';
             debugDiv.style.borderRadius = '5px';
             debugDiv.style.zIndex = '1000';
-            document.body.appendChild(debugDiv);
+            document.body.appendChild(debugDiv); */
             
             // Update debug info every 100ms
-            setInterval(() => {
+            /* setInterval(() => {
               debugDiv.textContent = `Gravity: x=${engine.gravity.x.toFixed(2)}, y=${engine.gravity.y.toFixed(2)}`;
-            }, 100);
+            }, 100); */
           } else {
             console.log('Permission denied');
             permissionButton.innerText = 'Permission Denied';
@@ -375,7 +375,7 @@ onMounted(() => {
       console.log('No permission needed, event listener added');
       
       // Add debug display for non-iOS too
-      const debugDiv = document.createElement('div');
+      /* const debugDiv = document.createElement('div');
       debugDiv.style.position = 'fixed';
       debugDiv.style.top = '10px';
       debugDiv.style.left = '10px';
@@ -388,7 +388,7 @@ onMounted(() => {
       
       setInterval(() => {
         debugDiv.textContent = `Gravity: x=${engine.gravity.x.toFixed(2)}, y=${engine.gravity.y.toFixed(2)}`;
-      }, 100);
+      }, 100); */
     }
   });
 
