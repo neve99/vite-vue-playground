585b775242beb1ab8660b12ff6ac3acf047f1f75
neve99 <165163178+neve99@users.noreply.github.com>
2025-03-15 19:20:08 +0100

    fix styling problems and add the cards

--- src/views/Cards.vue
+++ src/views/Cards.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="cards">
+  <div class="project-wrp">
     <nav>
       <div class="logo">
         <a href="#">Noir Wood</a>
@@ -21,7 +21,7 @@
       <p>© 2024 / 2025 Noir Wood</p>
     </footer>
     <div class="container">
-      <div class="slider">
+      <div class="slider" ref="sliderRef">
         <div class="card">
           <img src="/images/cards/01.jpg" alt="">
           <div class="copy">
@@ -58,7 +58,15 @@
 </template>
 
 <script setup>
+import { onMounted, ref } from 'vue'; 
 import gsap from 'gsap'
+
+const sliderRef = ref(null)
+
+onMounted(() => {
+  // log the width of the slider
+  console.log(sliderRef.value.clientWidth)
+})
 </script>
 
 <style scoped>
@@ -66,5 +74,95 @@ import gsap from 'gsap'
   margin: 0;
   padding: 0;
   box-sizing: border-box;
+}
+
+img{
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+p,
+a{
+  text-decoration: none;
+  font-weight: 600;
+  font-size: 14px;
+  color: #000;
+}
+
+nav{
+  position: absolute;
+  top: 0;
+  width: 100%;
+  padding: 2em;
+
+  display: flex;
+  align-items: center;
+
+  z-index: 2;
+}
+
+nav > div{
+  flex: 1;
+}
+
+.pages{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 0.5em;
+}
+
+.shop{
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  gap: 1em;
+}
+
+footer{
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 2em;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  gap: 1em;
+  z-index: 2;
+}
+
+.container {
+  position: relative;
+  width: 100%;
+  height: 100vh;
+  overflow: hidden;
+  background-color: #dfe1c8;
+}
+
+.slider {
+  position: absolute;
+  top: 15vh;
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+  perspective: 200px;
+  perspective-origin: 50% 100%;
+  display: flex;
+}
+
+.card{
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 50%;
+  height: 500px;
+  border-radius: 10px;
+  overflow: hidden;
+
+  transform: translate3d(-50%, -50%, 0);
+  background: #000;
 }
 </style>
