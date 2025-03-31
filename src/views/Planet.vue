e8e89126b2bbd4b733cf68f5db495788780f259e
neve99 <165163178+neve99@users.noreply.github.com>
2025-03-31 13:40:46 +0200

    ad make planet geometry and change some lights

--- src/views/Planet.vue
+++ src/views/Planet.vue
@@ -32,26 +32,45 @@ const initThree = () => {
   camera.lookAt(scene.position)
 
   // light
-  const light = new THREE.DirectionalLight(0xffffff, 1);
-  light.position.set(0, 0, -1);
-  scene.add(light);
+  const light = new THREE.DirectionalLight(0xffffff, 2);
+  const ambientLight = new THREE.AmbientLight(0xffffff, 1.5);
+  light.position.set(0, 1000, -1000);
+  scene.add(light, ambientLight);
 
   // renderer
   renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setSize(threeContainer.value.clientWidth, threeContainer.value.clientHeight);
   renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setClearColor(0x000000, 1);
+  renderer.setClearColor(0xf1f1f1, 1);
 
   // Add renderer to the DOM
   threeContainer.value.appendChild(renderer.domElement);
 
 
-
 }
 
 const animate = () => {
+  // Update the camera position
+  camera.lookAt(scene.position);
   animationFrameId = requestAnimationFrame(animate);
   renderer.render(scene, camera);
+}
+
+const makePlanet = () => {
+  // Create a sphere geometry with a radius of 800 and 128 segments, and put a material
+  const geometry = new THREE.SphereGeometry(800, 128, 128)
+  const material = new THREE.MeshStandardMaterial({
+    color: 0xffffff,
+    roughness: 0.5,
+    metalness: 0.5,
+    wireframe: false
+  })
+
+  // Create a mesh with the geometry and material, add it to the scene
+  const mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh)
+
+
 }
 
 // handle resize
@@ -80,15 +99,16 @@ const cleanup = () => {
 onMounted(() => {
   initThree();
 
+  
+  // Start the animation loop
+  animate();
+  makePlanet();
+  
   // Add event listeners
   window.addEventListener('resize', onResize);
   // window.addEventListener('mousemove', onMouseMove);
   // window.addEventListener('mousedown', onMouseDown);
   // window.addEventListener('mouseup', onMouseUp);
-
-  // Start the animation loop
-  animate();
-
 })
 
 onBeforeUnmount(() => {
